72fcec4af040d121c1b631f1cd3b89e42be92da1
rehan hawari <rehanhwr@rehanMBP.mshome.net>
2017-11-05 15:49:09 +0700

    add very basic landing page

--- home/views.py
+++ home/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from lab_1.views import mhs_name, birth_date
 
 #TODO Implement
 #Create a content paragraph for your landing page:
 landing_page_content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sollicitudin lacus vitae tortor consequat, vel ultricies nulla placerat. Sed vel suscipit arcu. Pellentesque et dolor ex. Donec blandit tortor at urna vestibulum luctus. Mauris tempus odio id dolor consectetur interdum. Vivamus id sem leo. Quisque at tincidunt odio.'
-
+mhs_name = 'rehan'
 def index(request):
     response = {'name': mhs_name, 'content': landing_page_content}
-    return render(request, 'index.html', response)
+    return render(request, 'home/index.html', response)
