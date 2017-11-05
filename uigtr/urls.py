8c131e7915f1124e834965782b4da046a2304b9f
rehan hawari <rehanhwr@rehanMBP.mshome.net>
2017-11-05 16:10:34 +0700

    fix footer

--- uigtr/urls.py
+++ uigtr/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic.base import RedirectView
-# from lab_1.views import index as index_lab1
 import home.urls as home
 
 
