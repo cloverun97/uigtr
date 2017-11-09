3248824e356aed812195d65e2d4ee64098a3c5d5
rehan hwr <rehan.hawari23@gmail.com>
2017-11-09 10:28:49 +0700

    MOVE TO PYTHONWEB (HOSTING)

--- home/views.py
+++ home/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 #TODO Implement
 #Create a content paragraph for your landing page:
-landing_page_content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sollicitudin lacus vitae tortor consequat, vel ultricies nulla placerat. Sed vel suscipit arcu. Pellentesque et dolor ex. Donec blandit tortor at urna vestibulum luctus. Mauris tempus odio id dolor consectetur interdum. Vivamus id sem leo. Quisque at tincidunt odio.'
+landing_page_content = 'DISINI KANLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sollicitudin lacus vitae tortor consequat, vel 
+ultricies 
+nulla placerat. Sed vel suscipit arcu. Pellentesque et dolor ex. Donec blandit tortor at urna vestibulum luctus. Mauris tempus odio id dolor consectetur interdum. Vivamus id sem leo. Quisque at tincidunt odio.'
 mhs_name = 'rehan'
 def index(request):
     response = {'name': mhs_name, 'content': landing_page_content}
