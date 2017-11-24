2af063bf9418998565d0e0d49c3f5e77bb90aa74
rehanhwr <rehan.hawari23@gmail.com>
2017-11-24 22:49:20 +0700

    change font navbar

--- home/static/js/home.js
+++ home/static/js/home.js
@@ -1,3 +1,19 @@
 $(window).on('load',function(){
      $('.loader').fadeOut();
 });
+
+$(document).ready(function() {
+	$(window).scroll(function() {
+	  	if($(document).scrollTop() > 10) {
+	    	$('#my-nav').addClass('scroll-navbar');
+	    }
+	    else {
+	    $('#my-nav').removeClass('scroll-navbar');
+	    }
+  	});
+
+ //  	$("#list").on("mouseover", function() {
+ //  		$(this).css( "color", "red" );
+	// });
+});
+
