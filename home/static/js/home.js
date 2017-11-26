04aeaff36d049e783ab7a0ffd69dff3c92b40abd
rehanhwr <rehan.hawari23@gmail.com>
2017-11-26 16:20:15 +0700

    add typing animation js

--- home/static/js/home.js
+++ home/static/js/home.js
@@ -1,21 +1,39 @@
 $(window).on('load',function(){
-     $('.loader').fadeOut();
+	$('.loader').fadeOut();
+	
+
+	//Welcoming text
+	var str= "Selamat Datang di Website UIGTR 2018!";
+	var split = str.split("");
+	var counter = 0;
+
+	var SI = setInterval(function(){
+		var h1 = $("#welcome");
+		h1.append(split[counter]);
+		counter++;
+		if (counter==str.length) {
+			clearInterval(SI)
+			$('#cursor').addClass("blinking-cursor");
+		}
+	}, 120); 
+
 });
 
 $(document).ready(function() {
+
+	//navbar scrolling 
 	$(window).scroll(function() {
 		var scroll = $(window).scrollTop();
-	  	if(scroll > 1) {
-	    	$('.my-navbar').css("background", "rgba(0, 0, 0, 0.5)");
-	    	$('.my-navbar').css("transition","all 0.5s");
-	    }
-	    else {
-	    $('.my-navbar').css("background", "none");
-	    }
-  	});
-	console.log(scroll);
- //  	$("#list").on("mouseover", function() {
- //  		$(this).css( "color", "red" );
-	// });
+		if(scroll > 1) {
+			$('.my-navbar').css("background", "rgba(0, 0, 0, 0.5)");
+			$('.my-navbar').css("transition","all 0.5s");
+		}
+		else {
+			$('.my-navbar').css("background", "none");
+		}
+	});
+
+
 });
 
+	
