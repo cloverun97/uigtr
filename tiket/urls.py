a987545ef3ca535436960d60ffe82c223be54360
rehanhwr <rehan.hawari23@gmail.com>
2017-12-15 22:29:55 +0700

    create ticket html and css

--- tiket/urls.py
+++ tiket/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import index
+from .views import index, beli_tiket
 #url for app
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'^beli/', beli_tiket, name='beli_tiket'),
 ]
--- tiket/views.py
+++ tiket/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
+	html = 'tiket/tiket.html'
+	response = {}
+	return render(request, html, response)
+
+def beli_tiket(request):
 	html = 'tiket/form_tiket.html'
 	response = {}
 	return render(request, html, response)
